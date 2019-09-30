b14c3c17d5db45ab287ba4edee435df46eee23e4
Nicolas Dufreche <dious@pm.me>
2019-09-30 23:01:39 +0200

    Using css variable

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
 import store from './store'
+import './assets/css/variables.css'
 import './assets/css/reset.css'
 import './assets/css/index.css'
 import App from './containers/app'
