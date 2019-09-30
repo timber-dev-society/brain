3015edf03a7ce41f673108a9c3d81dd2358c8ced
Nicolas Dufreche <n.dufreche@gmail.com>
2019-09-30 13:25:54 +0200

    Creating bookmark

---
+++ src/pages/bookmark-page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { IoMdBookmark }  from 'react-icons/io';
+
+
+const BookmarkPage = () => (
+  <div />
+)
+
+export default BookmarkPage
+export const url = '/bookmark'
+export const link = () => (<IoMdBookmark />)
