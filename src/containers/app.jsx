7c7c31f8d1ba8077f0841ad3a900be891c519c1d
Nicolas Dufreche <dious@pm.me>
2019-10-13 22:09:40 +0200

    animated background

--- src/containers/app.jsx
+++ src/containers/app.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import { Container, Row, Col } from 'reactstrap';
 
 import Page from './page'
 import Navbar from './navbar'
 import Pomodoro from './pomodoro'
 
+
 const App = () => (
-  <div className="container" style={{ backgroundColor: 'black', color: 'white', padding: '2% 10% 5%' }}>
-    <Navbar />
-    <Page />
+  <Container style={{ backgroundColor: 'black', color: 'white', padding: '2% 10% 5%' }}>
+    <Row>
+      <Col xs="3">
+        <Navbar />
+      </Col>
+      <Col xs="9">
+        <Page />
+      </Col>
+    </Row>
     <Pomodoro />
-  </div>
+  </Container>
 )
 
 export default App
