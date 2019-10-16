75c6f8279afaf4468ca3c08d109ab950e711f91c
Nicolas Dufreche <n.dufreche@gmail.com>
2019-10-16 14:40:10 +0200

    fixing test

--- src/__tests__/containers/app.test.js
+++ src/__tests__/containers/app.test.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+import store from './../../store'
+import App from './../../containers/app'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
