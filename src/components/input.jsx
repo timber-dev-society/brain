5296c5c5410485f0f8a2ca62f8239005524a76d6
Nicolas Dufreche <nicolas.dufreche@naturabuy.fr>
2020-10-30 20:48:20 +0100

    extebding base theme

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -3,6 +3,13 @@ import styled from 'styled-components'
 
 import { createTodo } from './../hooks/to-do'
 import { Input, Button } from './style'
+
+const InputEl = styled(Input)`
+  border-radius: 5px 0 0 5px;
+`
+const ButtonEl = styled(Button)`
+  border-radius: 0 5px 5px 0;
+`
 const Container = styled.div`
   margin: 0 auto;
   width: 860px;
@@ -13,11 +20,11 @@ const ToDoForm = () => {
 
   return (
     <Container>
-      <Input type='Primary' value={value} onChange={(event) => setValue(event.target.value)} />
-      <Button type='Primary' onClick={() => {
+      <InputEl type='Primary' value={value} onChange={(event) => setValue(event.target.value)} />
+      <ButtonEl type='Primary' onClick={() => {
         createTodo({ title: value })
         setValue('')
-      }}>add</Button>
+      }}>add</ButtonEl>
     </Container>
   )
 }
