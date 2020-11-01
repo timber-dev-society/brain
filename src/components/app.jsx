d3acb592ea5612bb8e3e6c2b1fa1056091b65e05
Noice Dious <dious@pm.me>
2020-11-01 10:33:40 +0100

    add inlinne editing

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Input from './input'
 import ToDoList from './to-do/list'
 import '../assets/style/reset.css'
 
+const Wrapper = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+`
+
 const App = () => (
-  <div>
+  <Wrapper>
     <Input />
     <ToDoList />
-  </div>
+  </Wrapper>
 )
 
 export default App
--- src/components/to-do/item.jsx
+++ src/components/to-do/item.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { Escape, Enter } from 'keyboard-key'
 
 import { todo } from './../../hooks/to-do'
 
@@ -7,12 +8,41 @@ const Wrapper = styled.div`
   padding-top: 10px;
   text-decoration: ${props => props.isCompleted === true ? 'line-through' : 'initial'}
 `
+const Title = styled.div`
+  display: inline-block;
+  margin-left: 10px;
+`
 
-const Todo = ({ id, title, isCompleted }) => (
-  <Wrapper isCompleted={isCompleted} onClick={ () => todo(id).toggle() }>
-    <span>#{id}</span>
-    <span>{title}</span>
-  </Wrapper>
-)
+const Id = styled.div`
+  display: inline-block;
+  width: 30px;
+  cursor: pointer;
+`
+
+const Todo = ({ id, title, isCompleted }) => {
+  const [ isEditing, setIsEditing ] = useState(false)
+  const [ value, setValue ] = useState(title)
+
+  const handleKeyDown = (event) => {
+    switch (event.keyCode) {
+      case Enter:
+        setIsEditing(false)
+        return todo(id).setTitle(value)
+      case Escape:
+        setIsEditing(false)
+        return setValue(title)
+      default:
+        return
+    }
+  }
+
+  return (
+    <Wrapper isCompleted={isCompleted}>
+      <Id onClick={ () => todo(id).toggle() }>#{id}</Id>
+      { isEditing || (<Title onClick={() => setIsEditing(true)}>{title}</Title>) }
+      { isEditing && (<input type="text" value={value} onChange={(ev) => setValue(ev.target.value)} onKeyDown={(ev) => handleKeyDown(ev)} />)}
+    </Wrapper>
+  )
+}
 
 export default Todo
